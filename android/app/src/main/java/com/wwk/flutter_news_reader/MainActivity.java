c2e1702bc90b52d42ceb314c3ee25b9aacccf4ec
Weikai Wang <wc563388548@gmail.com>
2019-03-18 17:42:32 +0900

    first commit

---
+++ android/app/src/main/java/com/wwk/flutter_news_reader/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wwk.flutter_news_reader;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
