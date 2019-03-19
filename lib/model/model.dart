6ffb0035086281142e981f5298986caca6b5aba3
Weikai Wang <wc563388548@gmail.com>
2019-03-19 19:01:13 +0900

    fix Model file for new screen

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,4 +1,32 @@
 // Create Data model for NewsApi
+
+class Article {
+  final Source source;
+  final String author;
+  final String title;
+  final String description;
+  final String url;
+  final String urlToImage;
+  final String publishedAt;
+  final String content;
+
+  Article({this.source, this.author, this.title, this.description, this.url, this.urlToImage, this.publishedAt, this.content});
+
+  factory Article.fromJson(Map<String, dynamic> json){
+    return Article(
+      source: Source.fromJsonForArticle(json['source']),
+      author: json['author'],
+      title: json['title'],
+      description: json['description'],
+      url: json['url'],
+      urlToImage: json['urlToImage'],
+      publishedAt: json['publishedAt'],
+      content: json['content'],
+    );
+  }
+
+}
+
 class Source {
   final String id;
   final String name;
@@ -19,6 +47,13 @@ class Source {
       category: json['category'],
       language: json['language'],
       country: json['country'],
+    );
+  }
+
+  factory Source.fromJsonForArticle(Map<String, dynamic> json){
+    return Source(
+      id: json['id'],
+      name: json['name'],
     );
   }
 }
